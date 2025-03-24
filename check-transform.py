318d6758b0b67c3c7cacd0115c73de4f865b1b47
Sunjoo Park <all4dich@gmail.com>
2025-03-24 17:42:01 +0900

    training/test sample program with FasionMNMIST

--- check-transform.py
+++ check-transform.py
@@ -1,4 +1,5 @@
 import os
+import time
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
@@ -9,26 +10,93 @@ if torch.accelerator.is_available():
     device = torch.accelerator.current_accelerator().type
 else:
     device = torch.device('cpu')
+print(f"Using {device} device")
+
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor()
+)
+
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor()
+)
+
+train_dataloader = DataLoader(training_data, batch_size=64)
+test_dataloader = DataLoader(test_data, batch_size=64)
 
 class NeuralNetwork(nn.Module):
-    def __init(self):
+    def __init__(self):
         super().__init__()
-        self.flatten = nn.Flatten
+        self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28 * 28, 512),
+            nn.Linear(28*28, 512),
             nn.ReLU(),
             nn.Linear(512, 512),
             nn.ReLU(),
             nn.Linear(512, 10),
         )
+
     def forward(self, x):
         x = self.flatten(x)
         logits = self.linear_relu_stack(x)
         return logits
 
 model = NeuralNetwork().to(device)
-X = torch.rand(1,28,28, device=device)
-logits = model(X)
-#pred_probab = nn.Softmax(dim=1)(logits)
-#y_pred = pred_probab.argmax(1)
-#print(f"Predicted class : {y_pred}")
+
+learning_rate = 1e-3
+batch_size = 64
+epochs = 5
+
+
+def train_loop(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X = X.to(device)
+        y= y.to(device)
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step() # Adjust the parameters by the gradients collected in the backward pass
+        optimizer.zero_grad() # Reset the gradients after updating the parameters
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), batch * batch_size + len(X)
+            print(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+
+def test_loopo(dataloader, model, loss_fn):
+    model.eval()
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test errro: \nAccuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f}\n")
+
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+epochs = 30
+start = time.time()
+for t in range(epochs):
+    print(f"Epoch {t+1}\n------------------------")
+    train_loop(train_dataloader, model, loss_fn, optimizer)
+    test_loopo(test_dataloader, model, loss_fn)
+
+end = time.time()
+print(f"Done! {end-start:.2f} seconds elapsed.")
