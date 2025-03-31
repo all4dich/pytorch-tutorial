246c63cac8cc3284413543a70e35f29a24422f86
Sunjoo Park <all4dich@gmail.com>
2025-03-31 11:10:06 +0900

    Update nn sample from scratch - use TensorDataset

--- learning-with-mnist.py
+++ learning-with-mnist.py
@@ -8,6 +8,7 @@ import torch
 import math
 import torch.nn.functional as F
 from torch import optim
+from torch.utils.data import TensorDataset
 
 DATA_PATH = Path("data")
 PATH = DATA_PATH / "mnist"
@@ -27,6 +28,8 @@ with gzip.open(PATH / FILENAME, "rb", "rb") as f:
 # pyplot.show()
 
 x_train, y_train, x_valid, y_valid = map(torch.tensor, (x_train, y_train, x_valid, y_valid))
+train_ds = TensorDataset(x_train, y_train)
+valid_ds = TensorDataset(x_valid, y_valid)
 
 n, c = x_train.shape
 weights = torch.randn(784, 10) / math.sqrt(784)
@@ -103,10 +106,7 @@ print(loss_func(model(xb), yb))
 def fit():
     for epoch in range(epochs):
         for i in range((n - 1) // bs + 1):
-            start_i = i * bs
-            end_i = start_i + bs
-            xb = x_train[start_i:end_i]
-            yb = y_train[start_i:end_i]
+            xb, yb = train_ds[i * bs: i * bs + bs]
             pred = model(xb)
             loss = loss_func(pred, yb)
 
