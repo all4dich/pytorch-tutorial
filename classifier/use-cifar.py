93fad602f2b9e7fb138f065184b7cc1fa1d14770
Sunjoo Park <all4dich@gmail.com>
2025-04-13 11:07:27 +0900

    Declare network class

--- classifier/use-cifar.py
+++ classifier/use-cifar.py
@@ -2,6 +2,10 @@ import torch
 import torchvision
 import torchvision.transforms as transform
 import os
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
 
 transform = transform.Compose([transform.ToTensor(), transform.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
@@ -15,14 +19,32 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-import matplotlib.pyplot as plt
-import numpy as np
 
 def imshow(img):
     img = img / 2 + 0.5 # Unnormalize
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        # you have to know the real size of images. CIFAR's image size is 32 x 32
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
 
 if __name__ == "__main__":
     dataiter = iter(trainloader)
