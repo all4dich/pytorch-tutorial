222ec126b392f19f04959a09a7fb781094815904
Sunjoo Park <all4dich@gmail.com>
2025-04-14 14:57:52 +0900

    RNN: Training/Evaluating

--- rnn/rnn-first.py
+++ rnn/rnn-first.py
@@ -1,5 +1,12 @@
 import torch
 from torch.utils.data import Dataset
+import torch.nn as nn
+import torch.nn.functional as F
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
 import string
 import unicodedata
 from io import open
@@ -10,14 +17,14 @@ import time
 device = torch.device("cpu")
 torch.set_default_device(device)
 print(f"Using device = {torch.get_default_device()}")
-
+# string.ascii_letters:   All allowed characters for ASCII
 allowed_characters = string.ascii_letters + " .,;'" + "_"
 n_letters = len(allowed_characters)
 
 
 def unicodeToAscii(s):
     return ''.join(
-        c for c in unicodedata.normalize('NFD', s)
+        c for c in unicodedata.normalize('NFD', s) # NFD: Normalized Form D
         if unicodedata.category(c) != 'Mn'
         and c in allowed_characters
     )
@@ -39,8 +46,10 @@ def lineToTensor(line):
         tensor[li][0][letterToIndex(letter)] = 1
     return tensor
 
-print (f"The letter 'a' becomes {lineToTensor('a')}") #notice that the first position in the tensor = 1
-print (f"The name 'Ahn' becomes {lineToTensor('Ahn')}") #notice 'A' sets the 27th index to 1
+
+print(f"The letter 'a' becomes {lineToTensor('a')}")  # notice that the first position in the tensor = 1
+print(f"The name 'Ahn' becomes {lineToTensor('Ahn')}")  # notice 'A' sets the 27th index to 1
+
 
 class NamesDataset(Dataset):
     def __init__(self, data_dir):
@@ -63,7 +72,7 @@ class NamesDataset(Dataset):
                 self.data_tensors.append(lineToTensor(name))
                 self.labels.append(label)
 
-        #Cache the tensor representation of the labels
+        # Cache the tensor representation of the labels
         self.labels_uniq = list(labels_set)
         for idx in range(len(self.labels)):
             temp_tensor = torch.tensor([self.labels_uniq.index(self.labels[idx])], dtype=torch.long)
@@ -80,7 +89,24 @@ class NamesDataset(Dataset):
 
         return label_tensor, data_tensor, data_label, data_item
 
-alldata= NamesDataset("data/names")
+
+class CharRNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(CharRNN, self).__init__()
+
+        self.rnn = nn.RNN(input_size, hidden_size)
+        self.h2o = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, line_tensor):
+        rnn_out, hidden = self.rnn(line_tensor)
+        output = self.h2o(hidden[0])
+        output = self.softmax(output)
+
+        return output
+
+
+alldata = NamesDataset("data/names")
 print(f"loaded {len(alldata)} items of data")
 print(f"example = {alldata[0]}")
 
@@ -88,3 +114,114 @@ train_set, test_set = torch.utils.data.random_split(alldata, [.85, .15])
 generator = torch.Generator(device=device).manual_seed(2024)
 
 print(f"train examples = {len(train_set)}, validation examples = {len(test_set)}")
+
+n_hidden = 128
+rnn = CharRNN(n_letters, n_hidden, len(alldata.labels_uniq))
+print(rnn)
+
+
+def label_from_output(output, output_labels):
+    top_n, top_i = output.topk(1)
+    label_i = top_i[0].item()
+    return output_labels[label_i], label_i
+
+
+input = lineToTensor('Albert')
+output = rnn(input)
+print(output)
+print(label_from_output(output, alldata.labels_uniq))
+
+
+def train(rnn, training_data, n_epoch = 10, n_batch_size = 64, report_every = 50, learning_rate = 0.2, criterion = nn.NLLLoss()):
+    """
+    Learn on a batch of training_data for a specified number of iterations and reporting thresholds
+    """
+    # Keep track of losses for plotting
+    current_loss = 0
+    all_losses = []
+    rnn.train()
+    optimizer = torch.optim.SGD(rnn.parameters(), lr=learning_rate)
+
+    start = time.time()
+    print(f"training on data set with n = {len(training_data)}")
+
+    for iter in range(1, n_epoch + 1):
+        rnn.zero_grad() # clear the gradients
+
+        # create some minibatches
+        # we cannot use dataloaders because each of our names is a different length
+        batches = list(range(len(training_data)))
+        random.shuffle(batches)
+        batches = np.array_split(batches, len(batches) //n_batch_size )
+
+        for idx, batch in enumerate(batches):
+            batch_loss = 0
+            for i in batch: #for each example in this batch
+                (label_tensor, text_tensor, label, text) = training_data[i]
+                output = rnn.forward(text_tensor)
+                loss = criterion(output, label_tensor)
+                batch_loss += loss
+
+            # optimize parameters
+            batch_loss.backward()
+            nn.utils.clip_grad_norm_(rnn.parameters(), 3)
+            optimizer.step()
+            optimizer.zero_grad()
+
+            current_loss += batch_loss.item() / len(batch)
+
+        all_losses.append(current_loss / len(batches) )
+        if iter % report_every == 0:
+            print(f"{iter} ({iter / n_epoch:.0%}): \t average batch loss = {all_losses[-1]}")
+        current_loss = 0
+
+    return all_losses
+
+start = time.time()
+all_losses = train(rnn, train_set, n_epoch=27, learning_rate=0.15, report_every=5)
+end = time.time()
+print(f"training took {end- start}s")
+
+
+plt.figure()
+plt.plot(all_losses)
+plt.show()
+
+
+def evaluate(rnn, testing_data, classes):
+    confusion = torch.zeros(len(classes), len(classes))
+
+    rnn.eval() #set to eval mode
+    with torch.no_grad(): # do not record the gradients during eval phase
+        for i in range(len(testing_data)):
+            (label_tensor, text_tensor, label, text) = testing_data[i]
+            output = rnn(text_tensor)
+            guess, guess_i = label_from_output(output, classes)
+            label_i = classes.index(label)
+            confusion[label_i][guess_i] += 1
+
+    # Normalize by dividing every row by its sum
+    for i in range(len(classes)):
+        denom = confusion[i].sum()
+        if denom > 0:
+            confusion[i] = confusion[i] / denom
+
+    # Set up plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(confusion.cpu().numpy()) #numpy uses cpu here so we need to use a cpu version
+    fig.colorbar(cax)
+
+    # Set up axes
+    ax.set_xticks(np.arange(len(classes)), labels=classes, rotation=90)
+    ax.set_yticks(np.arange(len(classes)), labels=classes)
+
+    # Force label at every tick
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    # sphinx_gallery_thumbnail_number = 2
+    plt.show()
+
+
+evaluate(rnn, test_set, classes=alldata.labels_uniq)
